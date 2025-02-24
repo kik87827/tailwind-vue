da88688e5933795e65cad43012def3f223f88479
kik <kik87827@gmail.com>
2025-02-24 18:26:43 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+
+
+<template>
+  <div>
+    <div class="grid grid-cols-2 gap-6">
+      <div class="shadow-sm bg-white rounded-lg">
+        shadow-sm
+      </div>
+      <div class="shadow bg-white rounded-lg">
+        shadow
+      </div>
+      <div class="shadow-md bg-white rounded-lg">
+        shadow
+      </div>
+      <div class="shadow-lg bg-white rounded-lg">
+        shadow
+      </div>
+      <div class="shadow-xl bg-white rounded-lg">
+        shadow
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="space-y-8 bg-blue-400">
+      <div class="w-96 bg-white shadow rounded">
+        w-96
+      </div>
+      <div class="w-80 bg-white shadow rounded">
+        w-80
+      </div>
+      <div class="w-64 bg-white shadow rounded">
+        w-64
+      </div>
+      <div class="w-60 bg-white shadow rounded">
+        w-60
+      </div>
+      <div class="w-1/2 bg-white shadow rounded">
+        w-60
+      </div>
+      <div class="w-3/4 bg-white shadow rounded">
+        w-60
+      </div>
+    </div>
+
+    <div class="text-base text-red-500">hello world</div>
+    <div class="text-lg">hello world</div>
+    <div class="text-2xl">hello world</div>
+    <div class="text-2xl p-3">hello world</div>
+    <div class="text-2xl pt-3">hello world</div>
+    <div class="text-2xl pb-3">hello world</div>
+    <div class="text-2xl pr-3">hello world</div>
+    <div class="text-2xl pl-3">hello world</div>
+
+    <br><br>
+
+    <div class="text-2xl mt-3 mb-3">hello world</div>
+    <div class="text-2xl mt-3">hello world</div>
+  </div>
+</template>
